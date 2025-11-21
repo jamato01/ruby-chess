62ee42b50aa8d23ee124b9f5fdd67b6564b69a46
jamato01 <jarrett.m.torres@gmail.com>
2025-11-21 18:31:55 -0500

    Add constants file

---
+++ lib/constants.rb
@@ -0,0 +1,14 @@
+module Chess
+  WHITE = 0
+  BLACK = 1
+
+  PIECE_TYPES = [:pawn, :knight, :bishop, :rook, :queen, :king]
+
+  # Move flags
+  QUIET = 0x0
+  CAPTURE = 0x1
+  DOUBLE_PAWN = 0x2
+  EN_PASSANT = 0x4
+  CASTLING = 0x8
+  PROMOTION = 0x10
+end
