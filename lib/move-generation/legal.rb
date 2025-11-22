32f7be5bb8a98ee3dfb7ccd27b0b2bc6ff38f493
jamato01 <jarrett.m.torres@gmail.com>
2025-11-21 21:00:40 -0500

    Add structure for legal move filter

---
+++ lib/move-generation/legal.rb
@@ -0,0 +1,12 @@
+module Chess
+  module MoveGenerator
+    module Legal
+      def self.generate(board)
+        moves = []
+        # gather pseudo-legal moves
+        # filter ones where king is in check after move
+        moves
+      end
+    end
+  end
+end
