8ca3ed0befdcd8b40b92874ebc6b46ef2a6636ac
jamato01 <jarrett.m.torres@gmail.com>
2025-11-21 20:57:07 -0500

    Add move object class

---
+++ lib/move.rb
@@ -0,0 +1,16 @@
+module Chess
+  class Move
+    attr_reader :from, :to, :flags, :promotion
+
+    def initialize(from:, to:, flags: 0, promotion: nil)
+      @from = from
+      @to = to
+      @flags = flags
+      @promotion = promotion
+    end
+
+    def capture?
+      flags & CAPTURE != 0
+    end
+  end
+end
