c32758aa22f4f3d49067ea3fd7a1b0b2b470faf3
jamato01 <jarrett.m.torres@gmail.com>
2025-11-22 22:24:04 -0500

    Add knight move generation

--- lib/move_generator/leaping.rb
+++ lib/move_generator/leaping.rb
@@ -1,7 +1,28 @@
 module Chess
   module MoveGenerator
     module Leaping
-      # Kight + King moves using lookup tables
+      # Kight moves using lookup tables
+      def generate_knight_moves(board, color)
+        pieces = color == WHITE ? board.white_knights : board.black_knights
+        
+        moves = []
+
+        Bitboard.each_bit(pieces) do |from|
+          attacks = LookupTables::KNIGHT_ATTACKS[from]
+
+          # Remove attacks on same-color pieces
+          attacks &= ~board.pieces(color)
+
+          Bitboard.each_bit(attacks) do |to|
+            moves << Move.new(from, to)
+          end
+        end
+
+        # All possible knight moves:
+        moves
+      end
+
+      # King moves using lookup tables.. also.. castling??????
     end
   end
 end
