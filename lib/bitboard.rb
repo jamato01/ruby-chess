08523dcafd00dd3fcdb23226225b3ae98f5a386e
jamato01 <jarrett.m.torres@gmail.com>
2025-11-22 21:44:10 -0500

    Add #bit_each method for iteration

--- lib/bitboard.rb
+++ lib/bitboard.rb
@@ -9,5 +9,14 @@ module Chess
       square = Math.log2(lsb).to_i
       [bb ^ lsb, square]
     end
+
+    def self.each_bit(bb)
+      while bb != 0
+        lsb = bb & -bb
+        square = Math.log2(lsb).to_i
+        yield square
+        bb &= bb - 1
+      end
+    end
   end
 end
