3e853601977f191f93fd8fbb13236e29184c1703
jamato01 <jarrett.m.torres@gmail.com>
2025-11-24 16:28:33 -0500

    Fix typos, make helper function private

--- lib/move_generator/legal.rb
+++ lib/move_generator/legal.rb
@@ -1,7 +1,7 @@
 module Chess
   module MoveGenerator
     module Legal
-      include Pawns, Sliding, Leaping
+      include Attacks, Pawns, Sliding, Leaping
       def self.generate(board)
         moves = []
         color = board.side_to_move
@@ -15,7 +15,7 @@ module Chess
         moves.each do |move|
           # Need to test the move on a cloned board
           temp_board = board.clone
-          MoveApplyer.apply(temp, move)
+          MoveApplier.apply(temp_board, move)
 
           # If king isn't in check, the move is legal and we can keep it
           legal_moves << move unless temp_board.in_check?(color)
@@ -23,7 +23,9 @@ module Chess
         legal_moves
       end
 
-      def self.generate_pseudo_legal(board, color)
+      private
+
+      def generate_pseudo_legal(board, color)
         moves = []
         moves.concat generate_pawn_moves(board, color)
         moves.concat generate_rook_moves(board, color)
@@ -31,6 +33,7 @@ module Chess
         moves.concat generate_queen_moves(board, color)
         moves.concat generate_knight_moves(board, color)
         moves.concat generate_king_moves(board, color)
+        moves
       end
     end
   end
