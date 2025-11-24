f6d3cbe40075f6e6753b8924efa7b39ed8768ace
jamato01 <jarrett.m.torres@gmail.com>
2025-11-24 14:19:54 -0500

    Add legal move generation
    Gather all pseudo-legal moves for side to move, then filter out moves
    that put side to move's king in check

--- lib/move_generator/legal.rb
+++ lib/move_generator/legal.rb
@@ -1,11 +1,36 @@
 module Chess
   module MoveGenerator
     module Legal
+      include Pawns, Sliding, Leaping
       def self.generate(board)
         moves = []
+        color = board.side_to_move
         # gather pseudo-legal moves
+        moves = generate_pseudo_legal(board, color)
+        
+        
         # filter ones where king is in check after move
-        moves
+        legal_moves = []
+        
+        moves.each do |move|
+          # Need to test the move on a cloned board
+          temp_board = board.clone
+          MoveApplyer.apply(temp, move)
+
+          # If king isn't in check, the move is legal and we can keep it
+          legal_moves << move unless temp_board.in_check?(color)
+        end
+        legal_moves
+      end
+
+      def self.generate_pseudo_legal(board, color)
+        moves = []
+        moves.concat generate_pawn_moves(board, color)
+        moves.concat generate_rook_moves(board, color)
+        moves.concat generate_bishop_moves(board, color)
+        moves.concat generate_queen_moves(board, color)
+        moves.concat generate_knight_moves(board, color)
+        moves.concat generate_king_moves(board, color)
       end
     end
   end
