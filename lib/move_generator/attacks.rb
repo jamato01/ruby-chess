d3de2e21ad7b17bb1bc8054da89d4a2b4a013376
jamato01 <jarrett.m.torres@gmail.com>
2025-11-24 15:49:44 -0500

    Add checks to see if a square is attacked

--- lib/move_generator/attacks.rb
+++ lib/move_generator/attacks.rb
@@ -52,6 +52,55 @@ module Chess
         # Just combine the bitboards of the attacks for rooks and bishops on the same square
         rook_attacks(square, blockers) | bishop_attacks(square, blockers)
       end
+
+      def self.square_attacked?(board, square, by_color)
+        return true if pawn_attacks_square?(board, square, by_color)
+        return true if knight_attacks_square?(board, square, by_color)
+        return true if king_attacks_square?(board, square, by_color)
+        return true if sliding_attacks_square?(board, square, by_color)
+      end
+
+      def self.pawn_attacks_square?(board, square, by_color)
+        pawn_attacks = by_color == WHITE ? WHITE_PAWN_ATTACKS[square] : BLACK_PAWN_ATTACKS[square]
+        pawn_bb = by_color == WHITE ? board.white_pawns : board.black_pawns
+        (pawn_attacks & pawn_bb) != 0
+      end
+
+
+      def self.knight_attacks_square?(board, square, by_color)
+        knight_bb = by_color == WHITE ? board.white_knights : board.black_knights
+        (KNIGHT_ATTACKS[square] & knight_bb) != 0
+      end
+
+      def self.king_attacks_square?(board, square, by_color)
+        king_bb = by_color == WHITE ? board.white_kings : board.black_kings
+        (KING_ATTACKS[square] & king_bb) != 0
+      end
+
+      def self.sliding_attacks_square?(board, square, by_color)
+        blockers = board.all_pieces
+        target_mask = 1 << square
+        # Bishop and Queen check
+        bishop_like = (color == WHITE ? 
+                      (board.white_bishops | board.white_queens) : 
+                      (board.black_bishops | board.black_queens))
+
+        # Rook and Queen check
+        rook_like   = (color == WHITE ?
+                      (board.white_rooks | board.white_queens) :
+                      (board.black_rooks | board.black_queens))
+
+        Bitboard.each_bit(bishop_like) do |from|
+          attacks = bishop_attacks(from, blockers)
+          return true if (attacks & target_mask) != 0
+        end
+
+        Bitboard.each_bit(rook_like) do |from|
+          attacks = rook_attacks(from, blockers)
+          return true if (attacks & target_mask) != 0
+        end
+        false
+      end
     end
   end
 end
