7c052b50db716fea4ce76e1071d240eadbaa8933
jamato01 <jarrett.m.torres@gmail.com>
2025-11-25 16:55:30 -0500

    Add en-passant unit tests

--- spec/en_passant_spec.rb
+++ spec/en_passant_spec.rb
@@ -39,3 +39,34 @@ describe 'En Passant capture via MoveApplier' do
     expect((result2.white_pawns & (1 << 28))).to eq(0)
   end
 end
+
+describe 'En Passant move generation' do
+  it 'generates an en-passant capture after an opponent double-pawn push' do
+    # White pawn on e5 (36), black pawn on d7 (51)
+    board = Chess::Board.new(
+      white_pawns: 1 << 36,
+      white_knights: 0,
+      white_bishops: 0,
+      white_rooks: 0,
+      white_queens: 0,
+      white_kings: 0,
+      black_pawns: 1 << 51,
+      black_knights: 0,
+      black_bishops: 0,
+      black_rooks: 0,
+      black_queens: 0,
+      black_kings: 0,
+      side_to_move: Chess::BLACK,
+      castling_rights: 0,
+      en_passant: nil
+    )
+
+    # Black double pawn push d7 -> d5 (51 -> 35)
+    double_move = Chess::Move.new(from: 51, to: 35, flags: Chess::DOUBLE_PAWN)
+    after = Chess::MoveApplier.apply(board, double_move)
+
+    # Now white pawn moves should include en-passant capture from e5 (36) to d6 (43)
+    moves = Chess::MoveGenerator::Pawns.generate_pawn_moves(after, Chess::WHITE)
+    expect(moves.any? { |m| m.from == 36 && m.to == 43 && (m.flags & Chess::EN_PASSANT) != 0 }).to be true
+  end
+end
