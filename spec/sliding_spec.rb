a98fc2895052ff461228a2e6e19e13e6b4806c81
jamato01 <jarrett.m.torres@gmail.com>
2025-11-25 16:28:42 -0500

    Add queen move generation unit tests

--- spec/sliding_spec.rb
+++ spec/sliding_spec.rb
@@ -47,45 +47,99 @@ describe Chess::MoveGenerator::Sliding do
     end
   end
 
-  def make_board_with_bishop(square)
-    Chess::Board.new(
-      white_pawns: 0,
-      white_knights: 0,
-      white_bishops: 1 << square,
-      white_rooks: 0,
-      white_queens: 0,
-      white_kings: 0,
-      black_pawns: 0,
-      black_knights: 0,
-      black_bishops: 0,
-      black_rooks: 0,
-      black_queens: 0,
-      black_kings: 0,
-      side_to_move: Chess::WHITE,
-      castling_rights: 0,
-      en_passant: nil
-    )
-  end
+  context 'Bishop moves' do
+    def make_board_with_bishop(square)
+      Chess::Board.new(
+        white_pawns: 0,
+        white_knights: 0,
+        white_bishops: 1 << square,
+        white_rooks: 0,
+        white_queens: 0,
+        white_kings: 0,
+        black_pawns: 0,
+        black_knights: 0,
+        black_bishops: 0,
+        black_rooks: 0,
+        black_queens: 0,
+        black_kings: 0,
+        side_to_move: Chess::WHITE,
+        castling_rights: 0,
+        en_passant: nil
+      )
+    end
 
-  it 'generates diagonal rook moves on empty board' do
-    board = make_board_with_bishop(27)
-    moves = Chess::MoveGenerator::Sliding.generate_bishop_moves(board, Chess::WHITE)
-    # expect a few directions reachable: a7 (48), b2 (9), g7 (54), g1 (6)
-    expect(moves.any? { |m| m.from == 27 && m.to == 48 }).to be true
-    expect(moves.any? { |m| m.from == 27 && m.to == 9 }).to be true
-    expect(moves.any? { |m| m.from == 27 && m.to == 54 }).to be true
-    expect(moves.any? { |m| m.from == 27 && m.to == 6 }).to be true
-  end
+    it 'generates diagonal rook moves on empty board' do
+      board = make_board_with_bishop(27)
+      moves = Chess::MoveGenerator::Sliding.generate_bishop_moves(board, Chess::WHITE)
+      # expect a few directions reachable: a7 (48), b2 (9), g7 (54), g1 (6)
+      expect(moves.any? { |m| m.from == 27 && m.to == 48 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 9 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 54 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 6 }).to be true
+    end
 
-  it 'generates diagonal bishop moves with blockers' do
+    it 'generates diagonal bishop moves with blockers' do
       board = make_board_with_bishop(27)
       # Black Rook on b6 (41), White Rook on e3 (20)
       board.add_piece(Chess::BLACK, :rook, 41)
       board.add_piece(Chess::WHITE, :rook, 20)
-    moves = Chess::MoveGenerator::Sliding.generate_bishop_moves(board, Chess::WHITE)
+      moves = Chess::MoveGenerator::Sliding.generate_bishop_moves(board, Chess::WHITE)
       # expect b6 (41) to be reachable, but a7 (48) and e3 (20) to not be reachable
       expect(moves.any? { |m| m.from == 27 && m.to == 41 }).to be true
       expect(moves.any? { |m| m.from == 27 && m.to == 48 }).to be false
       expect(moves.any? { |m| m.from == 27 && m.to == 20 }).to be false
     end
+  end
+
+  context 'Queen moves' do
+    def make_board_with_queen(square)
+      Chess::Board.new(
+        white_pawns: 0,
+        white_knights: 0,
+        white_bishops: 0,
+        white_rooks: 0,
+        white_queens: 1 << square,
+        white_kings: 0,
+        black_pawns: 0,
+        black_knights: 0,
+        black_bishops: 0,
+        black_rooks: 0,
+        black_queens: 0,
+        black_kings: 0,
+        side_to_move: Chess::WHITE,
+        castling_rights: 0,
+        en_passant: nil
+      )
+    end
+
+    it 'generates orthogonal and diagonal queen moves on empty board' do
+      board = make_board_with_queen(27)
+      moves = Chess::MoveGenerator::Sliding.generate_queen_moves(board, Chess::WHITE)
+      # expect all moves from rook and bishop tests to be generated
+      expect(moves.any? { |m| m.from == 27 && m.to == 48 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 9 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 54 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 6 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 19 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 59 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 26 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 28 }).to be true
+    end
+
+    it 'generates orthogonal and diagonal queen moves with blockers' do
+      board = make_board_with_queen(27)
+      board.add_piece(Chess::BLACK, :rook, 41)
+      board.add_piece(Chess::WHITE, :rook, 20)
+      board.add_piece(Chess::BLACK, :bishop, 43)
+      board.add_piece(Chess::WHITE, :bishop, 25)
+      moves = Chess::MoveGenerator::Sliding.generate_queen_moves(board, Chess::WHITE)
+      expect(moves.any? { |m| m.from == 27 && m.to == 41 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 48 }).to be false
+      expect(moves.any? { |m| m.from == 27 && m.to == 20 }).to be false
+      expect(moves.any? { |m| m.from == 27 && m.to == 35 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 43 }).to be true
+      expect(moves.any? { |m| m.from == 27 && m.to == 51 }).to be false
+      expect(moves.any? { |m| m.from == 27 && m.to == 25 }).to be false
+    end
+  end
 end
