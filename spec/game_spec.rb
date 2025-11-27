ad12361a33f2944823c681cf61b3a0b95e4eed28
jamato01 <jarrett.m.torres@gmail.com>
2025-11-26 20:42:46 -0500

    Add tests for save/load and force resigns/draws

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -32,3 +32,45 @@ describe Chess::Game do
     end
   end
 end
+
+describe 'Game save/load and forced results' do
+  require 'tmpdir'
+
+  it 'saves and loads a game' do
+    game = Chess::Game.new(Chess::Board.start_position)
+    path = File.join(Dir.tmpdir, "ruby_chess_test_save_#{Time.now.to_i}.sav")
+    game.save(path)
+    expect(File.exist?(path)).to be true
+    loaded = Chess::Game.load(path)
+    expect(loaded).to be_a(Chess::Game)
+    # loaded game should have a board and position_count
+    expect(loaded.position_count).to be >= 1
+    # cleanup
+    File.delete(path) if File.exist?(path)
+  end
+
+  it 'allows resign and force_draw and exposes winner/over?' do
+    game = Chess::Game.new(Chess::Board.start_position)
+    expect(game.over?).to be false
+    # resigning should mark game over and set winner to opponent
+    side = game.board.side_to_move
+    game.resign(side)
+    expect(game.over?).to be true
+    expect(game.winner).not_to be_nil
+
+    # force draw
+    g2 = Chess::Game.new(Chess::Board.start_position)
+    g2.force_draw
+    expect(g2.over?).to be true
+    expect(g2.winner).to be_nil
+  end
+
+  it 'reports threefold repetition when position count reaches 3' do
+    game = Chess::Game.new(Chess::Board.start_position)
+    # simulate two more occurrences of the same position
+    key = game.board.position_key
+    game.instance_variable_get(:@position_counts)[key] += 2
+    expect(game.position_count).to be >= 3
+    expect(game.threefold_repetition?).to be true
+  end
+end
